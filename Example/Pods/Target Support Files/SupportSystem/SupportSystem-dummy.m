a4c5151fa68548fe346b73195fb392cb8b28e694
uzair-whizpool <uzair.masood@whizpool.com>
2022-07-07 12:45:37 +0500

    initial commit

---
+++ Example/Pods/Target Support Files/SupportSystem/SupportSystem-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SupportSystem : NSObject
+@end
+@implementation PodsDummy_SupportSystem
+@end
--- Example/SupportSystem/ViewController.swift
+++ Example/SupportSystem/ViewController.swift
@@ -7,18 +7,42 @@
 //
 
 import UIKit
+//import SupportSystem
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // initilization and create Folder in Directory
+        SLog.shared.initilization()
+        
+        // Write Logs in Logs File with message
+        SLog.shared.log(text: "Hello Buddy")
+        SLog.shared.log(text: "Hi")
+        SLog.shared.log(text: "Yes Please")
+        SLog.shared.log(text: "No")
+        
+        // function Textview Editing Calls
+        SLog.shared.setpassword(password: "QWERTY")
+        
+        // set tittle
+        SLog.shared.setTittle(title: "Map App")
+        
+        // set days
+        SLog.shared.setDaysForLog(numberOfDays: 8)
+        
+        // set tag
+        SLog.shared.setDefaultTag(tagName: "Logs")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func ClickBtn(_ sender: UIButton) {
+        
+        SLog.shared.getLogFilePath { zipPath in
+            //
+            print("zippath: \(zipPath)")
+        }
+        
+        
     }
-
+    
 }
 
